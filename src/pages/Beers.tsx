2f9723ffd1b6685dcd3cffedbdf592e73c42e191
Arash Cipriano Cardoso Kaffashi <arash_kaffashi@hotmail.com>
2022-11-16 01:26:59 -0400

    done: iteration 7

--- src/pages/Beers.tsx
+++ src/pages/Beers.tsx
@@ -8,6 +8,7 @@ import { beer } from "../interfaces";
 export default function Beers() {
   const navigate = useNavigate();
   const [beers, setBeers] = useState([] as beer[]);
+  const [searchItem, setSearchItem] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -20,25 +21,43 @@ export default function Beers() {
     navigate("/beers/" + beer._id, { state: beer, replace: false });
   }
 
+  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchItem(e.target.value);
+  }
+
   return (
     <div>
-      {beers.map((beer) => {
-        let { image, name, tagline, contributed_by, _id } = beer;
-        return (
-          <div className="beer" key={_id} onClick={() => handleClick(beer)}>
-            <div>
-              <img src={image} alt="tagline" />
+      <div className="search">
+        <label htmlFor="search">Search</label>
+        <input
+          type="text"
+          id="search"
+          value={searchItem}
+          onChange={handleSearch}
+        />
+      </div>
+      {beers
+        .filter((beer) =>
+          beer.name.toLowerCase().includes(searchItem.toLowerCase())
+        )
+        .map((beer) => {
+          let { image, name, tagline, contributed_by, _id } = beer;
+          return (
+            <div className="beer" key={_id} onClick={() => handleClick(beer)}>
+              <div>
+                <img src={image} alt="tagline" />
+              </div>
+              <div>
+                <h1>{name}</h1>
+                <h2>{tagline}</h2>
+                <p>
+                  <span className="created_by">Created by:</span>{" "}
+                  {contributed_by}
+                </p>
+              </div>
             </div>
-            <div>
-              <h1>{name}</h1>
-              <h2>{tagline}</h2>
-              <p>
-                <span className="created_by">Created by:</span> {contributed_by}
-              </p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
